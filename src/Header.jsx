21cd3cfa58b105658ddfee93756edd6331988ff2
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-06-10 01:03:09 +0100

    Making progress on the Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,22 +6,25 @@ const Header = ()=> {
 
 
   return (
-    <div className='container d-flex justify-content-center align-items-center'>
-      <div className='nav'>
-        <header>
-        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleSidebar}>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
+    <div className="container-fluid d-flex align-items-center" style={{backgroundColor:'#191b1d'}}>
+      <div className="row justify-content-between m-3 w-100">
+        <div className="nav">
+          <header>
+            <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleSidebar}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </header>
+          <h3 className="p-2">PLAYER MARKET</h3>
         </div>
-      </header>
-      <h3>PLAYER MARKET</h3>
       </div>
   
-      <div className='balance'>
-        <h4>Balance : 1700</h4>
+      <div className="balance d-flex m-4" style={{ marginLeft: 'auto' }}>
+      <h4 className="me-1">Balance:</h4>
+        <h4>1700</h4>
       </div>
-
+  
       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
         <nav>
           <ul>
@@ -32,14 +35,16 @@ const Header = ()=> {
           </ul>
         </nav>
       </div>
-
+  
       {isOpen && <div className="overlay" onClick={closeSidebar} />}
-      
+  
       <div className={`content ${isOpen ? 'shifted' : ''}`}>
         {/* Add your content here */}
       </div>
     </div>
   );
+  
+  
 }
 
 export default Header;
