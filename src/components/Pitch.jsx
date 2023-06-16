1a0c45b331883bf5938fbf0c5658a121a3f6a15d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-06-17 00:15:05 +0100

    Started pinpointing the coordinates

--- src/components/Pitch.jsx
+++ src/components/Pitch.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import pitch from '../images/pitch.svg';
 import Position from './Position';
 import Team from './Team';
@@ -7,35 +7,57 @@ import Formations from './Formations';
 
 const Pitch = () => {
   const pitchImageRef = useRef(null);
+  const [pitchWidth, setPitchWidth] = useState(0);
+  const [pitchHeight, setPitchHeight] = useState(0);
 
   useEffect(() => {
-    if (pitchImageRef.current) {
-      const width = pitchImageRef.current.clientWidth;
-      const height = pitchImageRef.current.clientHeight;
-      console.log('Rendered Pitch Width:', width);
-      console.log('Rendered Pitch Height:', height);
-    }
+    const updateDimensions = () => {
+      if (pitchImageRef.current) {
+        const width = pitchImageRef.current.clientWidth;
+        const height = pitchImageRef.current.clientHeight;
+        setPitchWidth(width);
+        setPitchHeight(height);
+        console.log(width)
+        console.log(height)
+      }
+    };
+
+    updateDimensions(); // Initial dimensions
+
+    window.addEventListener('resize', updateDimensions); // Add event listener for window resize
+
+    return () => {
+      window.removeEventListener('resize', updateDimensions); // Clean up the event listener on component unmount
+    };
   }, []);
 
   const renderPlayerImages = () => {
     const players = [
-      { x: 30, y: 25, image: './src/images/placeholder.png' },
-      { x: 50, y: 45, image: './src/images/placeholder.png' },
+      { x: 50, y: 18, image: './src/images/placeholder.png' }, //GK
+      { x: 27, y: 34, image: './src/images/placeholder.png' },
+      { x: 73, y: 34, image: './src/images/placeholder.png' },
+      { x: 73, y: 34, image: './src/images/placeholder.png' },
+      { x: 73, y: 34, image: './src/images/placeholder.png' },
       // Add more players with their respective x, y coordinates and image URLs
     ];
 
-    const CLICK = ()=>{
-      console.log("CLICKED")
-    }
+    const calculateSize = (size) => {
+      return (size * pitchWidth) / 100; // Convert the percentage size to pixels based on pitch width
+    };
+
+    const handleClick = () => {
+      console.log('CLICKED');
+    };
 
     return players.map((player, index) => (
-      <img onClick={CLICK}
+      <img
+        onClick={handleClick}
         key={index}
         src={player.image}
-        width={'45px'}
+        width={calculateSize(7) + 'px'} // Example: 5% width of the pitch
         alt={`Player ${index + 1}`}
         style={{
-          cursor:'pointer',
+          cursor: 'pointer',
           position: 'absolute',
           left: `${player.x}%`,
           top: `${player.y}%`,
